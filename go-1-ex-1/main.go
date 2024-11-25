08493404d343e700637d0489c23d8a214c8ecd89
Riccardo <riccardo.greco@xappido.com>
2024-11-25 08:43:18 +0100

    ex 1: Declared und initialized variables

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	firstName := "Riccardo"
+	lastName := "Greco"
+	dayOfBirth := 31
+	monthOfBirth := 07
+	yearOfBirth := 2008
+	numberOfSiblings := 2
+	heightInMeters := 1.86
+	zodiacSign := 'L' 
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
