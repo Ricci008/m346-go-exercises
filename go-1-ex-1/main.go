2c7945e37e3c676a83595fbb05c123c8b8d0ca4b
Riccardo <riccardo.greco@xappido.com>
2024-11-25 09:15:40 +0100

    ex 1: changed decleration

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,14 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	firstName := "Riccardo"
-	lastName := "Greco"
-	dayOfBirth := 31
-	monthOfBirth := 07
-	yearOfBirth := 2008
-	numberOfSiblings := 2
-	heightInMeters := 1.86
-	zodiacSign := 'L' 
+	var firstName string = "Riccardo"
+	var lastName string = "Greco"
+
+	var dayOfBirth byte = 31
+	var monthOfBirth byte = 07
+	var yearOfBirth int16 = 2008
+
+	var numberOfSiblings byte = 2
+
+	var heightInMeters float64 = 1.86
+
+	var zodiacSign rune = '\u264c'
+
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
