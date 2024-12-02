321b61b9880277d8f98f5e2630e9741ad121359f
Riccardo <riccardo.greco@xappido.com>
2024-12-02 08:11:58 +0100

    ex 3: Add file output for dice roll results

--- go-1-ex-3/main.go
+++ go-1-ex-3/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -10,12 +11,11 @@ func main() {
 	var eyes = rand.Intn(5) + 1
 	var when = time.Now()
 
-	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice shows", eyes, "eyes")
-
-	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice was rolled at", when)
+	// Write to eyes.txt
+	fmt.Fprintln(os.Stdout, "the dice shows", eyes, "eyes")
+	// Write to dice.log
+	fmt.Fprintln(os.Stderr, "the dice was rolled at", when)
 
 	// TODO: how to write the output into eyes.txt and dice.log?
-	// go run ex3/main.go TODO
+	// go run ex3/main.go > eyes.txt 2> dice.log
 }
