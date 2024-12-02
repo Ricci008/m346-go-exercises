50f78336f8fcde3a984f03666d2d0527e6ce964d
Riccardo <riccardo.greco@xappido.com>
2024-12-02 09:12:50 +0100

    2-ex1: defined variable and used struct

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -4,25 +4,44 @@ import "fmt"
 
 type FullName struct {
 	// TODO: add fields
+	FirstName string
+	LastName  string
 }
 
 // TODO: declare a structure for birth date
+type BirthDate struct {
+	Day   int
+	Month int
+	Year  int
+}
 
 type Profile struct {
 	// TODO: embed full name and birth date information
+	Name             FullName
+	Birth            BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
 
 func main() {
 	var me = Profile{
+		Name: FullName{
+			FirstName: "Riccardo",
+			LastName:  "Greco",
+		},
+		Birth: BirthDate{
+			Day:   31,
+			Month: 07,
+			Year:  2008,
+		},
 		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		NumberOfSiblings: 2,        // TODO: adjust
+		ZodiacSign:       '\u264c', // TODO: adjust
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
 	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings++
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
