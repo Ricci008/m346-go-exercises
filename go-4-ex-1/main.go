7130985b74658691ad970b9a359133d78c1de78c
Riccardo <riccardo.greco@xappido.com>
2024-12-09 08:17:43 +0100

    4-ex-1: Implement computeGrade function

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,15 @@
 package main
 
+import "fmt"
+
 // TODO: implement the function computeGrade
+func computeGrade(gotPoints, maxPoints float64) float64 {
+	return (gotPoints/maxPoints)*5 + 1
+}
 
 func main() {
 	// TODO: call the function computeGrade
+	fmt.Println(computeGrade(17.5, 28.0))
+	fmt.Println(computeGrade(25.0, 30.0))
+	fmt.Println(computeGrade(07.0, 20.0))
 }
